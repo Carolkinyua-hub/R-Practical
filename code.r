c97a51ee6e3a3bc270318bc18386b3f0a5e80cc0
Carolkinyua-hub <carolnkinyua@gmail.com>
2021-11-19 11:24:40 +0300

    add visualization

--- code.r
+++ code.r
@@ -6,8 +6,18 @@ load("/home/carol/Desktop/R-Code/Practice/marketing.rda")
 # view data for analysis
 View(marketing)
 # transform data into a dataframe
-marketingdf <-data.frame(marketing)
-View(marketingdf)  
-ls(marketingdf)
-marketing_Channel<-c("facebook","youtube","newspaper")
-View(marketing_Channel)
+df<-data.frame(marketing)
+View(df)
+library(ggplot2)
+View(df)
+install.packages("tidyverse")
+# visualizing the impact of facebook activity to sales
+ggplot(data=df, aes(x=facebook, y=sales))+ geom_point()
+
+# visualizing the impact of youtube activity to sales
+ggplot(data=df, aes(x=youtube, y=sales))+ geom_point()
+# visualizing the impact of newspaper ads on sales
+ggplot(data=df, aes(x=newspaper, y=sales))+geom_point()
+
+
+
