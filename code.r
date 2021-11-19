eb4f7a6bb532bd22a77e577bbb411e5036daca9f
Carolkinyua-hub <carolnkinyua@gmail.com>
2021-11-19 13:19:43 +0300

    add XT_X times Y

--- code.r
+++ code.r
@@ -38,6 +38,9 @@ View(df2)
 #Creating the Response Variable
 df3<-select(df, -facebook,-youtube,-newspaper)
 View(df3)
+# converting Df3 to Y matrix
+Y<-data.matrix(df3)
+View(df3)
 #Creating the first column of ones
 ones<-rep (1, times=200)
 View(ones)
@@ -58,4 +61,10 @@ View(X2_T)
 X_new<-X2_T %*% X2
 View(X_new)
 # Computing the inverse
-
+Xinv<-inv(X_new)
+# view inverse
+View(Xinv)
+# Compute for X transpose Y
+X2T_Y<-X2_T %*% Y
+View(X2T_Y)
+# compute OLS B1-Bk
