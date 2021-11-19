9e239c8f8f873dfcd6d017ab87127c77720923b7
Carolkinyua-hub <carolnkinyua@gmail.com>
2021-11-19 11:37:21 +0300

    add code geom_method(lm)

--- code.r
+++ code.r
@@ -8,16 +8,22 @@ View(marketing)
 # transform data into a dataframe
 df<-data.frame(marketing)
 View(df)
+# Accessing ggplot for visualization
 library(ggplot2)
 View(df)
-install.packages("tidyverse")
+# Re-installing tidyverse for analysis purposes
+install.packages("tidyverse" , dependencies = "TRUE")
+# Accessing tidyverse
+library(tidyverse)
+# Accessing(dplyr)
+library(dplyr)
 # visualizing the impact of facebook activity to sales
-ggplot(data=df, aes(x=facebook, y=sales))+ geom_point()
-
+ggplot(data=df, aes(x=facebook, y=sales))+ geom_point()+geom_smooth(method="lm")
 # visualizing the impact of youtube activity to sales
-ggplot(data=df, aes(x=youtube, y=sales))+ geom_point()
+ggplot(data=df, aes(x=youtube, y=sales))+ geom_point()+geom_smooth(method="lm")
 # visualizing the impact of newspaper ads on sales
-ggplot(data=df, aes(x=newspaper, y=sales))+geom_point()
-
+ggplot(data=df, aes(x=newspaper, y=sales))+geom_point()+geom_smooth(method="lm")
+# Running multiple regression based of the 3 variables and their impact on sales
+ggplt(data=df)
 
 
